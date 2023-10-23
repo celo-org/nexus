d9a4ef3d65d0811bdf2177add78fc9220a05e384
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-10-23 15:47:17 +0530

    fix: request user to connect wallet first

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { encodeFunctionData, parseEther, toHex } from "viem";
-import { usePublicClient, useWalletClient } from "wagmi";
+import { useAccount, usePublicClient, useWalletClient } from "wagmi";
 import FederatedAttestationsAbi from "../abi/FederatedAttestation";
 import stableTokenAbi from "../abi/StableToken";
 import toast from "react-hot-toast";
@@ -11,6 +11,7 @@ const ISSUER_ADDRESS = "0xDF7d8B197EB130cF68809730b0D41999A830c4d7";
 
 export default function Home() {
     const publicClient = usePublicClient();
+    const { isConnected } = useAccount();
     const { data: walletClient } = useWalletClient();
     const [resolvedReceiverAddress, setResolvedReceiverAddress] = useState("");
     const [identifier, setIdentifier] = useState("");
@@ -98,47 +99,60 @@ export default function Home() {
     return (
         <div className="w-screen flex flex-col justify-center items-center gap-y-4">
             <p className="text-xl">Send cUSD to external users</p>
-            <input
-                className="text-[48px] bg-gypsum outline-none text-center"
-                type="number"
-                value={transferValue}
-                onChange={({ target }) => setTransferValue(target.value)}
-                placeholder="0"
-            />
-            <p className="font-bold">cUSD</p>
-            <div className="flex items-center gap-x-4">
-                <span>To:</span>
-                <input
-                    value={identifier}
-                    onChange={({ target }) => setIdentifier(target.value)}
-                    className="text-[16px] w-[250px] bg-gypsum border-b-2 outline-none"
-                />
-            </div>
-            {resolvedReceiverAddress ? (
-                <div className="flex flex-col gap-y-4 items-center">
-                    <p>Resolved Address: {resolvedReceiverAddress}</p>
-                    <button
-                        onClick={sendCUSD}
-                        className="rounded-md border border-black bg-prosperity px-3 py-1 text-base font-medium text-black shadow-sm"
-                    >
-                        Confirm
-                    </button>
-                </div>
-            ) : (
+            {isConnected ? (
                 <>
-                    <button
-                        onClick={lookupAddress}
-                        className="rounded-md border border-black bg-prosperity px-3 py-1 text-base font-medium text-black shadow-sm"
-                    >
-                        Send
-                    </button>
-                    {/* <button
-                        onClick={register}
-                        className="rounded-md border border-black bg-prosperity px-3 py-1 text-base font-medium text-black shadow-sm"
-                    >
-                        Register
-                    </button> */}
+                    <input
+                        className="text-[48px] bg-gypsum outline-none text-center"
+                        type="number"
+                        value={transferValue}
+                        onChange={({ target }) =>
+                            setTransferValue(target.value)
+                        }
+                        placeholder="0"
+                    />
+                    <p className="font-bold">cUSD</p>
+                    <div className="flex items-center gap-x-4">
+                        <span>To:</span>
+                        <input
+                            value={identifier}
+                            onChange={({ target }) =>
+                                setIdentifier(target.value)
+                            }
+                            className="text-[16px] w-[250px] bg-gypsum border-b-2 outline-none"
+                        />
+                    </div>
+                    {resolvedReceiverAddress ? (
+                        <div className="flex text-center flex-col gap-y-4 items-center">
+                            <p>Resolved Address: {resolvedReceiverAddress}</p>
+                            <button
+                                onClick={sendCUSD}
+                                className="rounded-md border border-black bg-prosperity px-3 py-1 text-base font-medium text-black shadow-sm"
+                            >
+                                Confirm
+                            </button>
+                        </div>
+                    ) : (
+                        <>
+                            <button
+                                onClick={lookupAddress}
+                                className="rounded-md border border-black bg-prosperity px-3 py-1 text-base font-medium text-black shadow-sm"
+                            >
+                                Send
+                            </button>
+                            {/* <button
+                            onClick={register}
+                            className="rounded-md border border-black bg-prosperity px-3 py-1 text-base font-medium text-black shadow-sm"
+                        >
+                            Register
+                        </button> */}
+                        </>
+                    )}
+                    <p className="text-center">
+                        Issuer Address: {ISSUER_ADDRESS}
+                    </p>
                 </>
+            ) : (
+                <p>Please connect wallet first</p>
             )}
         </div>
     );
